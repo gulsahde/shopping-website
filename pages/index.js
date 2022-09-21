a6854fde5a7692b71e97368e12c444e9ef293b9e
GulsahDgr <34721537+GulsahDgr@users.noreply.github.com>
2022-09-21 16:05:18 +0300

    second

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!--hello again</a>
+          Welcome to <a href="https://nextjs.org">Next.js!--hello again77</a>
         </h1>
 
         <p className={styles.description}>
